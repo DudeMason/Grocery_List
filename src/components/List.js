a1b4477dfa60e0d544154d98a67681407a96f63d
Gabriella Algecera <gabriellaalgecera@Brians-MBP.domain>
2019-12-17 16:17:44 -0700

    fix merge conflict

--- src/components/List.js
+++ src/components/List.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import Item from './Item';
-import Name from './Name';
+import Item from './Item'
+import Name from './Name'
 
-<<<<<<< HEAD
-const List = ({ items, completeItems}) => (
-  <div>
-  <h1 style={{textDecorationLine: 'underline'}}><Name /></h1>
-=======
 const List = ({ items, completeItems, removeItem, toggleEdit, edit }) => (
   <div>
   <h1 style={{textDecorationLine: 'underline'}}>
-    Grocery List
+   < Name />
     <button onClick={toggleEdit} style={{}}>Edit</button>
   </h1>
->>>>>>> 87785f9d7a00d080aae2eec6f8eadbf3a82eb5d9
   <ul style={{ padding: '10px 60px 1px 10px', fontSize: '15pt'}}>
     { items.map (item =>
       <Item
