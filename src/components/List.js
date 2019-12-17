91f4757ad100c3507e32bee88474ad5bd4902285
Gabriella Algecera <gabriellaalgecera@Brians-MBP.domain>
2019-12-17 16:10:26 -0700

    add name component

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Item from './Item'
+import Item from './Item';
+import Name from './Name';
 
-const List = ({ items, completeItems, editItems }) => (
+const List = ({ items, completeItems}) => (
   <div>
-  <h1 style={{textDecorationLine: 'underline'}}>Grocery List</h1>
+  <h1 style={{textDecorationLine: 'underline'}}><Name /></h1>
   <ul style={{ padding: '10px 60px 1px 10px', fontSize: '15pt'}}>
     { items.map (item => <Item key={item.id} {...item} completeItems={completeItems}/> )}
   </ul>
